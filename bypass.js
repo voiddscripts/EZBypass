d05410bc94c3af90f9d551d409ec11e8af7ee193
voidd <94987124+voiddscripts@users.noreply.github.com>
2023-06-18 15:48:36 +0200

    Update bypass.js

--- bypass.js
+++ bypass.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         EZBypass (Linkvertise) [Updated again]
+// @name         EZBypass (Linkvertise and Krnl Linkvertise) [Updated again]
 // @namespace    http://tampermonkey.net/
-// @version      1.3
+// @version      1.5
 // @description  Simplifies Linkvertise link bypassing.
 // @author       X1Y
 // @match        *://*.linkvertise.com/*
@@ -24,16 +24,25 @@
   'use strict';
 
   const bypassServerUrl = 'https://bypass.pm/bypass2?url=' + encodeURIComponent(window.location.href);
+  const targetLink = 'https://linkvertise.com/48193/';
 
   async function bypassLinkvertise() {
     const headers = {
       'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
     };
 
+    if (window.location.href.includes(targetLink)) {
+      await delay(16000);
+    }
+
     const response = await fetch(bypassServerUrl, { headers });
     const data = await response.json();
     const link = data.destination;
     window.location.href = link;
+  }
+
+  function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
 
   bypassLinkvertise();
